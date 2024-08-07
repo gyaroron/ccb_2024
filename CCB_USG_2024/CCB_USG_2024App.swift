c702089efdf393b2cb356b230cea2cf18852a11a
m1mac2021 <m1mac2021@m1mac2021s-MacBook-Air.local>
2024-08-07 09:00:01 +0900

    rebase repo

---
+++ CCB_USG_2024/CCB_USG_2024App.swift
@@ -0,0 +1,17 @@
+//
+//  CCB_USG_2024App.swift
+//  CCB_USG_2024
+//
+//  Created by m1mac2021 on 8/7/24.
+//
+
+import SwiftUI
+
+@main
+struct CCB_USG_2024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
