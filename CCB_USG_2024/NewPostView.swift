e4c1050ec6e51fb13db0ebdf902baebe6f912c8e
user <robinhood12481024@gmail.com>
2024-08-13 21:30:25 +0900

    newpost

---
+++ CCB_USG_2024/NewPostView.swift
@@ -0,0 +1,18 @@
+//
+//  NewPostView.swift
+//  CCB_USG_2024
+//
+//  Created by user on 8/13/24.
+//
+
+import SwiftUI
+
+struct NewPostView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewPostView()
+}
